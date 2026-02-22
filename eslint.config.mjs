4e1c0b191d29d48f745c4678064fa276e9ec0b15
Filip Zrnzevic <filip.zrnzevic@gmail.com>
2026-02-22 18:34:57 +0100

    feat: create Bloom Next.js bouquet app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+  }),
+  {
+    ignores: [
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ]),
-]);
+    ],
+  },
+];
 
 export default eslintConfig;
